97d6fdac7dd7d72540edb3e6e354e45110babba7
박민현 <algusl92@naver.com>
2017-04-29 17:42:54 +0900

    통신을 위한 charThread Class 추가

--- TCPClient_Blinker/app/src/main/java/com/example/blackman/tcpclient_blinker/MainActivity.java
+++ TCPClient_Blinker/app/src/main/java/com/example/blackman/tcpclient_blinker/MainActivity.java
@@ -1,13 +1,93 @@
 package com.example.blackman.tcpclient_blinker;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** Called when the activity is first created. */
+    public Socket cSocket = null;
+    private String server = "172.30.1.19";         // 서버 ip주소
+    private int port = 9000;                           // 포트번호
+
+    public PrintWriter streamOut = null;
+    public BufferedReader streamIn = null;
+
+    public chatThread cThread = null;
+
+    public TextView tv;
+    public EditText nickText;
+    public EditText msgText;
+    public ScrollView sv;
+
+    public String nickName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void logger(String MSG) {
+        tv.append(MSG + "\n");     // 텍스트뷰에 메세지를 더해줍니다.
+        sv.fullScroll(ScrollView.FOCUS_DOWN); // 스크롤뷰의 스크롤을 내려줍니다.
+    }
+
+    Handler mHandler = new Handler() {   // 스레드에서 메세지를 받을 핸들러.
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0: // 채팅 메세지를 받아온다.
+                    logger(msg.obj.toString());
+                    break;
+                case 1: // 소켓접속을 끊는다.
+                    try {
+                        cSocket.close();
+                        cSocket = null;
+
+                        logger("접속이 끊어졌습니다.");
+
+                    } catch (Exception e) {
+                        logger("접속이 이미 끊겨 있습니다." + e.getMessage());
+                        finish();
+                    }
+                    break;
+            }
+        }
+    };
+
+    class chatThread extends Thread {
+        private boolean flag = false; // 스레드 유지(종료)용 플래그
+        public void run() {
+            try {
+                while (!flag) { // 플래그가 false일경우에 루프
+                    String msgs;
+                    Message msg = new Message();
+                    msg.what = 0;
+                    msgs = streamIn.readLine();  // 서버에서 올 메세지를 기다린다.
+                    msg.obj = msgs;
+
+                    mHandler.sendMessage(msg); // 핸들러로 메세지 전송
+
+                    if (msgs.equals("# [" + nickName + "]님이 나가셨습니다.")) { // 서버에서 온 메세지가 종료 메세지라면
+                        flag = true;   // 스레드 종료를 위해 플래그를 true로 바꿈.
+                        msg = new Message();
+                        msg.what = 1;   // 종료메세지
+                        mHandler.sendMessage(msg);
+                    }
+                }
+
+            }catch(Exception e) {
+                logger(e.getMessage());
+            }
+        }
     }
 }
